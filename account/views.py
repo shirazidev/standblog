aa99b7e0828f41a642202b359607e79cc27e3a0d
AmirHossein Shirazi {ShiraziDEV} <ashzih2@gmail.com>
2023-10-25 15:21:21 +0330

    deploy login v 0.1.0

--- account/views.py
+++ account/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
 
 
-def login(request):
+# from .models import
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return redirect("/login/")
+    else:
+        pass
     return render(request, 'account/index.html')
