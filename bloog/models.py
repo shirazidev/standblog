a15214f60835d9728efa8c0d2849d94bf88c8a0a
AmirHossein Shirazi <ashzih2@gmail.com>
2023-11-29 00:32:45 +0330

    researching about diffrent ForeignKeys

--- bloog/models.py
+++ bloog/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#models.cascade +> abshary
+#models.set_null => null = true => vaghti on model hazf she bejaye on user/object null tarif mishe
+# blank = true => mitonim to panel modiriat ye field ro optional konim.
+# models.set_default, default=1  => vaghti useri ro pak mikonim kole article haye oon user be user1 montaghel mishe
+# models.protect => hefazat mikone az user az tarighe article haei ke dare va nemizare user delete she ta vaghti ke article ha pak nashode
+# cascade
+# set_null
+
+# protect
+# set default
+
+# do_nothing
+
 class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=70)
