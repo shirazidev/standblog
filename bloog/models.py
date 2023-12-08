a2c0f4e5c6da829bea6a7f62d244a692f57365d2
AmirHossein Shirazi <ashzih2@gmail.com>
2023-12-08 11:47:38 +0330

    some changes

--- bloog/models.py
+++ bloog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 #models.cascade +> abshary
 #models.set_null => null = true => vaghti on model hazf she bejaye on user/object null tarif mishe
 # blank = true => mitonim to panel modiriat ye field ro optional konim.
@@ -16,11 +16,12 @@ from django.contrib.auth.models import User
 
 class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=70)
+    title = models.CharField(max_length=70, unique_for_date="pub_date")
     body = models.TextField()
     image = models.ImageField(upload_to="images/articles")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateField(default=timezone.now())
 
     def __str__(self):
         return f"{self.title} - {self.body[:30]} - Updated at: {self.updated}"
