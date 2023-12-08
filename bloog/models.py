4f67ba9a976bf5aef04dad200de2d72d5c11158b
AmirHossein Shirazi <ashzih2@gmail.com>
2023-12-08 18:52:32 +0330

    adding article category

--- bloog/models.py
+++ bloog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-#models.cascade +> abshary
-#models.set_null => null = true => vaghti on model hazf she bejaye on user/object null tarif mishe
+
+
+# models.cascade +> abshary
+# models.set_null => null = true => vaghti on model hazf she bejaye on user/object null tarif mishe
 # blank = true => mitonim to panel modiriat ye field ro optional konim.
 # models.set_default, default=1  => vaghti useri ro pak mikonim kole article haye oon user be user1 montaghel mishe
 # models.protect => hefazat mikone az user az tarighe article haei ke dare va nemizare user delete she ta vaghti ke article ha pak nashode
@@ -14,14 +16,22 @@ from django.utils import timezone
 
 # do_nothing
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=70, unique_for_date="pub_date")
+    category = models.ManyToManyField(Category)
+    title = models.CharField(max_length=70)
     body = models.TextField()
     image = models.ImageField(upload_to="images/articles")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
-    pub_date = models.DateField(default=timezone.now())
 
     def __str__(self):
         return f"{self.title} - {self.body[:30]} - Updated at: {self.updated}"
